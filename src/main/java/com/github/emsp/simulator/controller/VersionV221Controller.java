ef288d3a764421627f77ba24812c580afc1893cc
ifnu <ifnubima@gmail.com>
2025-01-14 13:03:41 +0700

    set data to have multiple versions

--- src/main/java/com/github/emsp/simulator/controller/VersionV221Controller.java
+++ src/main/java/com/github/emsp/simulator/controller/VersionV221Controller.java
@@ -1,5 +1,8 @@
 package com.github.emsp.simulator.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,13 @@ public class VersionV221Controller {
     
 
     @GetMapping("/ocpi/emsp/2.2.1/versions")
-    public ResponseEntity<Response<Version>> getVersion211(){
+    public ResponseEntity<Response<List<Version>>> getVersion221(){
         //validate Token A
-        Response<Version> versionResponse = new Response<>();
-        versionResponse.setData(new Version("2.2.1", "http://103.127.97.27:8081/ocpi/emsp/2.2.1/"));
+        Response<List<Version>> versionResponse = new Response<>();
+        List<Version> versions = new ArrayList<>();
+        versions.add(new Version("2.1.1", "http://103.127.97.27:8081/ocpi/emsp/2.1.1/"));
+        versions.add(new Version("2.2.1", "http://103.127.97.27:8081/ocpi/emsp/2.2.1/"));
+        versionResponse.setData(versions);
         return ResponseEntity.ok().body(versionResponse);
 
     }
