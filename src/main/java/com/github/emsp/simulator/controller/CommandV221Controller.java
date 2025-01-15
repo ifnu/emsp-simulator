9cf5bc8652f2a6f6f23d3c193bd1a54170f93989
ifnu <ifnubima@gmail.com>
2025-01-15 11:35:27 +0700

    add commands module url for eMSP

--- src/main/java/com/github/emsp/simulator/controller/CommandV221Controller.java
+++ src/main/java/com/github/emsp/simulator/controller/CommandV221Controller.java
@@ -1,18 +1,18 @@
 package com.github.emsp.simulator.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.emsp.simulator.model.ResponseNoData;
 
 @RestController
-public class CredentialV211Controller {
+public class CommandV221Controller {
 
-    @GetMapping("/ocpi/emsp/2.1.1/credentials")
-    public ResponseEntity<ResponseNoData> getCredentials211() {
+    @PostMapping("/ocpi/emsp/2.2.1/commands/{commandType}")
+    public ResponseEntity<ResponseNoData> geCredentials211(@PathVariable String commandType) {
         ResponseNoData response = new ResponseNoData();
         return ResponseEntity.ok().body(response);
     }
-
 }
---
+++ src/main/java/com/github/emsp/simulator/model/ResponseNoData.java
@@ -0,0 +1,34 @@
+package com.github.emsp.simulator.model;
+
+import java.util.Date;
+
+public class ResponseNoData {
+    private String statusCode = "1000"; //success
+    private String statusMessage = "Success";
+    private Date timestamp = new Date();
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
