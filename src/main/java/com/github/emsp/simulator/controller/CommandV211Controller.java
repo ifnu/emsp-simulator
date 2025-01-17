0306e8056a1d8db9077d86863f8c712344cb78a1
ifnu <ifnubima@gmail.com>
2025-01-17 10:55:25 +0700

    capture request body when handling command callback

--- src/main/java/com/github/emsp/simulator/controller/CommandV211Controller.java
+++ src/main/java/com/github/emsp/simulator/controller/CommandV211Controller.java
@@ -3,16 +3,21 @@ package com.github.emsp.simulator.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.emsp.simulator.model.CommandResponse;
+import com.github.emsp.simulator.model.CommandResult;
 import com.github.emsp.simulator.model.Response;
 
 @RestController
 public class CommandV211Controller {
 
     @PostMapping("/ocpi/emsp/2.1.1/commands/{commandType}/{uid}")
-    public ResponseEntity<Response<CommandResponse>> postCommand(@PathVariable String commandType, @PathVariable String uid) {
+    public ResponseEntity<Response<CommandResponse>> postCommand(
+            @RequestBody CommandResult result,
+            @PathVariable String commandType,
+            @PathVariable String uid) {
         Response<CommandResponse> response = new Response<>();
         CommandResponse commandResponse = new CommandResponse();
         commandResponse.setResult("ACCEPTED");
---
+++ src/main/java/com/github/emsp/simulator/model/CommandResult.java
@@ -0,0 +1,14 @@
+package com.github.emsp.simulator.model;
+
+public class CommandResult {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+}
