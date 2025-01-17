3fc924267a4d531f40c0ee229dfc614f55e49de0
ifnu <ifnubima@gmail.com>
2025-01-17 10:46:06 +0700

    add command url

--- src/main/java/com/github/emsp/simulator/controller/CommandV211Controller.java
+++ src/main/java/com/github/emsp/simulator/controller/CommandV211Controller.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.emsp.simulator.model.ResponseNoData;
+import com.github.emsp.simulator.model.CommandResponse;
+import com.github.emsp.simulator.model.Response;
 
 @RestController
 public class CommandV211Controller {
 
     @PostMapping("/ocpi/emsp/2.1.1/commands/{commandType}")
-    public ResponseEntity<ResponseNoData> geCredentials211(@PathVariable String commandType) {
-        ResponseNoData response = new ResponseNoData();
+    public ResponseEntity<Response<CommandResponse>> postCommand(@PathVariable String commandType) {
+        Response<CommandResponse> response = new Response<>();
+        CommandResponse commandResponse = new CommandResponse();
+        commandResponse.setResult("ACCEPTED");
         return ResponseEntity.ok().body(response);
     }
 }
---
+++ src/main/java/com/github/emsp/simulator/model/CommandResponse.java
@@ -0,0 +1,14 @@
+package com.github.emsp.simulator.model;
+
+public class CommandResponse {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+}
