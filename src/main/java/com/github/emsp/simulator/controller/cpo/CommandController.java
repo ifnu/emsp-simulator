192a794ec7d5c7da226c3540c4277d5f4a68e0fd
ifnu <ifnubima@gmail.com>
2025-02-08 21:24:18 +0700

    add uid, insert when CPO url called

--- src/main/java/com/github/emsp/simulator/controller/cpo/CommandController.java
+++ src/main/java/com/github/emsp/simulator/controller/cpo/CommandController.java
@@ -1,5 +1,6 @@
 package com.github.emsp.simulator.controller.cpo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,16 +8,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.emsp.simulator.entity.Request;
 import com.github.emsp.simulator.model.cpo.LocationRemoteStartDto;
 import com.github.emsp.simulator.model.cpo.RemoteCommandResult;
+import com.github.emsp.simulator.repository.RequestRepository;
 
 @RestController
 public class CommandController {
+    @Autowired
+    private RequestRepository repository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @PostMapping("/v1/locations/{locationId}/remotestartinternal")
     public ResponseEntity<RemoteCommandResult> postStartSession(
             @RequestBody LocationRemoteStartDto requestBody,
             @PathVariable String locationId) {
+
+        Request request = new Request();
+        request.setModule("commands remotestartinternal");
+        request.setParty("eMSP");
+        request.setVersion("ocpi v2.1.1");
+        request.setUid(requestBody.getUid());
+        try {
+            request.setData(objectMapper.writeValueAsString(requestBody));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repository.save(request);
 
         RemoteCommandResult response = new RemoteCommandResult();
         response.setType("START_SESSION");
@@ -30,6 +51,19 @@ public class CommandController {
             @PathVariable String sessionId,
             @RequestParam String uniqueId) {
 
+        Request request = new Request();
+        request.setModule("commands remotestopinternal");
+        request.setParty("eMSP");
+        request.setVersion("ocpi v2.1.1");
+        request.setUid(uniqueId);
+        try {
+            request.setData("sessionId:" + sessionId + ", locationId:" + locationId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repository.save(request);
+
         RemoteCommandResult response = new RemoteCommandResult();
         response.setType("STOP_SESSION");
         response.setIsOk(true);
@@ -42,6 +76,19 @@ public class CommandController {
             @PathVariable String connectorId,
             @RequestParam String uniqueId) {
 
+        Request request = new Request();
+        request.setModule("commands unlockconnectorinternal");
+        request.setParty("eMSP");
+        request.setVersion("ocpi v2.1.1");
+        request.setUid(uniqueId);
+        try {
+            request.setData("connectorId:" + connectorId + ", locationId:" + locationId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repository.save(request);
+        
         RemoteCommandResult response = new RemoteCommandResult();
         response.setType("UNLOCK_CONNECTOR");
         response.setIsOk(true);
--- src/main/java/com/github/emsp/simulator/controller/emsp/CommandV221Controller.java
+++ src/main/java/com/github/emsp/simulator/controller/emsp/CommandV221Controller.java
@@ -1,22 +1,45 @@
 package com.github.emsp.simulator.controller.emsp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.emsp.simulator.entity.Request;
 import com.github.emsp.simulator.model.emsp.CommandResult;
 import com.github.emsp.simulator.model.emsp.ResponseNoData;
+import com.github.emsp.simulator.repository.RequestRepository;
 
 @RestController
 public class CommandV221Controller {
 
+    @Autowired
+    private RequestRepository repository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    
     @PostMapping("/ocpi/emsp/2.2.1/commands/{commandType}/{uid}")
     public ResponseEntity<ResponseNoData> postCommand(
-            @RequestBody CommandResult commandResult,
+            @RequestBody CommandResult result,
             @PathVariable String commandType,
             @PathVariable String uid) {
+
+        Request request = new Request();
+        request.setModule("commands " + commandType);
+        request.setParty("eMSP");
+        request.setVersion("ocpi v2.1.1");
+        request.setUid(uid);
+        try {
+            request.setData(objectMapper.writeValueAsString(result));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        repository.save(request);
+
         ResponseNoData response = new ResponseNoData();
         return ResponseEntity.ok().body(response);
     }
