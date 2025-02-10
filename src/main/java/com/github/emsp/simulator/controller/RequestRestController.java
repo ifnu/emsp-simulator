7104fd6acbc4ffa9be7fd63183abf9a207ba08f0
ifnu <ifnubima@gmail.com>
2025-02-10 13:47:38 +0700

    add clear requests button

--- src/main/java/com/github/emsp/simulator/controller/RequestRestController.java
+++ src/main/java/com/github/emsp/simulator/controller/RequestRestController.java
@@ -3,6 +3,7 @@ package com.github.emsp.simulator.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,11 @@ public class RequestRestController {
     @GetMapping("/requests")
     public List<Request> getAll() {
         return repository.findByOrderByDateDesc();
+    }
+
+    @DeleteMapping("/requests")
+    public void deleteAll() {
+        repository.deleteAll();
     }
 
 }
